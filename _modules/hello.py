3a5f63f5e04a7f00280d3953b87aaf730fc53162
Gareth J. Greenaway <gareth@wiked.org>
2019-04-05 13:20:03 -0700

    initial check in

---
+++ _modules/hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+A simple module to give a greeting
+'''
+
+from __future__ import absolute_import, unicode_literals, print_function
+
+
+def world():
+    '''
+    Say Hello
+    '''
+    return "Hello World!"
